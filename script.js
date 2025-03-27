73e33dc87aedf5e94c5c6b74e1d821b43d146f05
Elias Bariani <Elias>
2025-03-27 16:48:41 -0300

    pou versão 2.0

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ let clicks = 0;
 let multiplicador = 1;
 let bossMode = false;
 let bossTimer;
+let timerInterval;
 
 function clicar() {
-    clicks += multiplicador;
-    document.getElementById("contador").innerText = clicks;
-    document.getElementById("pou").style.transform = "scale(1.1)";
-    setTimeout(() => document.getElementById("pou").style.transform = "scale(1)", 100);
+
+        clicks += multiplicador;
+        document.getElementById("contador").innerText = clicks;
+        document.getElementById("pou").style.transform = "scale(1.1)";
+        setTimeout(() => document.getElementById("pou").style.transform = "scale(1)", 100);
 }
 
 function comprarMultiplicador(fator, custo) {
@@ -26,7 +28,7 @@ function comprarRoupa(custo, imagem) {
         document.getElementById("contador").innerText = clicks;
         document.getElementById("pou").src = imagem;
         
-        if (imagem === 'pou_666.avif') {
+        if (imagem === 'pou_boss.avif') {
             iniciarBossMode();
         }
     } else {
@@ -37,17 +39,39 @@ function comprarRoupa(custo, imagem) {
 function iniciarBossMode() {
     if (bossMode) return;
     bossMode = true;
-    alert("Você tem 10 segundos para alcançar 5000 clicks ou perderá!");
+    document.getElementById("timer").style.display = "block";
+
+    alert("A batalha contra o Pou666 começou! Você tem 10 segundos para derrotá-lo! Se você acumular mais de 5000 cliques, você vencerá!");
+
+    let tempoRestante = 10;
+    document.getElementById("timer").innerText = tempoRestante;
+    
+    timerInterval = setInterval(() => {
+        tempoRestante--;
+        document.getElementById("timer").innerText = tempoRestante;
+        if (tempoRestante <= 0) {
+            clearInterval(timerInterval);
+        }
+    }, 1000);
+    
     bossTimer = setTimeout(() => {
+        document.getElementById("timer").style.display = "none";
         if (clicks < 5000) {
             alert("Você perdeu! Pou666 dominou!");
-            bossMode = false;
+            resetarJogo();
         } else {
             alert("Parabéns! Você derrotou Pou666 e se tornou um Deus!");
-            document.getElementById("pou").src = 'pou_god.PNG';
+            document.getElementById("pou").src = 'pou_7.PNG';
             clicks += 100000;
             document.getElementById("contador").innerText = clicks;
-            bossMode = false;
         }
+        bossMode = false;
     }, 10000);
+}
+
+function resetarJogo() {
+    document.getElementById("pou").src = 'pou_0.webp';
+    clicks = 0;
+    multiplicador = 1;
+    document.getElementById("contador").innerText = clicks;
 }
